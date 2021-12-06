573add17c3f779edcfef17c8b7e8ed80f39696b6
Bilal Shafiq <mrbilalshafiq@gmail.com>
2021-12-06 14:24:32 +0000

    Edited Triannglearea.py

--- 9. TriangleArea.py
+++ 9. TriangleArea.py
@@ -1,5 +1,8 @@
 #Calculate area of a triangle
-side1, side2, side3 = eval(input("Enter length of sides: "))
+side1 = eval(input("Enter length of sides: "))
+side2 = eval(input("Enter length of sides: "))
+side3 = eval(input("Enter length of sides: "))
+
 s = (side1 + side2 + side3) / 2
 area = (s * (s - side1) * (s - side2) * (s - side3)) ** 0.5
 
