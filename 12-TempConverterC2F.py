5b1515feefdc00ec4eb4659fb028a6366443ef84
Bilal Shafiq <75343545+mrbilalshafiq@users.noreply.github.com>
2021-12-07 17:31:03 +0000

    Delete 12-TempConverterC2F.py

--- 12-TempConverterC2F.py
+++
@@ -1,6 +0,0 @@
-
-#Converting Celsius to Fahrenheit
-celsius = eval(input("Enter temperature in degrees celsius: "))
-fahrenheit = (9 / 5) * celsius + 32
-
-print(celsius, "Celsius is", fahrenheit, "Fahrenheit")
